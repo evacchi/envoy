5f8da7e8f73016140d46bc606d4bbaa3be4d081e
Edoardo Vacchi <evacchi@users.noreply.github.com>
2024-06-18 17:41:09 +0200

    example
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

--- examples/golang-http/simple/example.go
+++ examples/golang-http/simple/example.go
@@ -7,9 +7,8 @@ import (
 
 func myHttpHandler(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
-	if path == "/localreply_by_config" {
-		body := fmt.Sprintf("%s, path: %s\r\n", "f.config.echoBody", path)
+	if path == "/localreply" {
+		body := fmt.Sprintf("hello from path: %s\r\n", path)
 		w.Write([]byte(body))
 	}
-
 }
--- examples/golang-http/simple/filter.go
+++ examples/golang-http/simple/filter.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/envoyproxy/envoy/contrib/golang/common/go/api"
 	"net/http"
-	"strconv"
 )
 
 var UpdateUpstreamBody = "upstream response body updated by the simple plugin"
@@ -15,135 +14,63 @@ type filter struct {
 	api.PassThroughStreamFilter
 
 	callbacks api.FilterCallbackHandler
-	path      string
 	config    *config
 
-	handler http.Handler
-}
-
-func (f *filter) sendLocalReplyInternal() api.StatusType {
-	// Remember to return LocalReply when the request is replied locally
-	return api.LocalReply
+	handler        http.Handler
+	request        *http.Request
+	responseWriter *responseWriter
 }
 
 // Callbacks which are called in request path
 // The endStream is true if the request doesn't have body
 func (f *filter) DecodeHeaders(header api.RequestHeaderMap, endStream bool) api.StatusType {
-	headers := http.Header{}
 
-	method, _ := header.Get(":method")
-	scheme, _ := header.Get(":scheme")
-	auth, _ := header.Get(":authority")
-	path, _ := header.Get(":path")
+	r := headersToRequest(header)
+	rw := newResponseWriter()
+	f.request, f.responseWriter = r, rw
 
-	r, err := http.NewRequest(method, fmt.Sprintf("%s://%s%s", scheme, auth, path), nopReader{})
+	f.handler.ServeHTTP(rw, r)
+	grpcStatus := int64(0)
+	// If the ResponseWriter has been modified, then we can handle the response locally.
+	if rw.modified {
+		f.callbacks.DecoderFilterCallbacks().SendLocalReply(rw.status, rw.buf.String(), rw.header, grpcStatus, "")
+		return api.LocalReply
+	}
+	// Write back changes in the request into the headers.
+	requestToHeaders(r, header)
+	return api.Continue
+}
+
+func headersToRequest(header api.RequestHeaderMap) *http.Request {
+	url := extractUrl(header)
+
+	r, err := http.NewRequest(header.Method(), url, nopReader{})
 	if err != nil {
 		panic(err)
 	}
 
-	header.Range(func(key, value string) bool {
-		k := http.CanonicalHeaderKey(key)
-		headers[k] = append(headers[k], value)
-		return true
-	})
-
-	rw := newResponseWriter()
-	if endStream {
-		f.handler.ServeHTTP(rw, r)
-		grcpStatus := int64(0)
-		// fixme: check response writer
-		if rw.status == 0 {
-			rw.status = http.StatusOK
-		}
-		f.callbacks.DecoderFilterCallbacks().SendLocalReply(rw.status, rw.buf.String(), rw.header, grcpStatus, "lol")
-		return api.LocalReply
-	} else {
-		return api.Continue
-	}
-
+	r.Header = header.GetAllHeaders()
+	return r
 }
 
-// DecodeData might be called multiple times during handling the request body.
-// The endStream is true when handling the last piece of the body.
-func (f *filter) DecodeData(buffer api.BufferInstance, endStream bool) api.StatusType {
-	// support suspending & resuming the filter in a background goroutine
-	return api.Continue
+func extractUrl(header api.RequestHeaderMap) string {
+	return fmt.Sprintf("%s://%s%s", header.Scheme(), header.Host(), header.Path())
 }
 
-func (f *filter) DecodeTrailers(trailers api.RequestTrailerMap) api.StatusType {
-	// support suspending & resuming the filter in a background goroutine
-	return api.Continue
-}
+func requestToHeaders(req *http.Request, header api.RequestHeaderMap) {
+	header.SetHost(req.URL.Host)
+	header.SetPath(req.URL.Path)
+	header.SetMethod(req.Method)
 
-// Callbacks which are called in response path
-// The endStream is true if the response doesn't have body
-func (f *filter) EncodeHeaders(header api.ResponseHeaderMap, endStream bool) api.StatusType {
-	if f.path == "/update_upstream_response" {
-		header.Set("Content-Length", strconv.Itoa(len(UpdateUpstreamBody)))
-	}
-	header.Set("Rsp-Header-From-Go", "bar-test")
-	// support suspending & resuming the filter in a background goroutine
-	return api.Continue
-}
-
-// EncodeData might be called multiple times during handling the response body.
-// The endStream is true when handling the last piece of the body.
-func (f *filter) EncodeData(buffer api.BufferInstance, endStream bool) api.StatusType {
-	if f.path == "/update_upstream_response" {
-		if endStream {
-			buffer.SetString(UpdateUpstreamBody)
-		} else {
-			buffer.Reset()
+	for k, v := range req.Header {
+		header.Del(k)
+		for i, vv := range v {
+			if i == 0 {
+				header.Set(k, vv)
+			} else {
+				header.Add(k, vv)
+			}
 		}
 	}
-	// support suspending & resuming the filter in a background goroutine
-	return api.Continue
-}
 
-func (f *filter) EncodeTrailers(trailers api.ResponseTrailerMap) api.StatusType {
-	return api.Continue
-}
-
-// OnLog is called when the HTTP stream is ended on HTTP Connection Manager filter.
-func (f *filter) OnLog() {
-	code, _ := f.callbacks.StreamInfo().ResponseCode()
-	respCode := strconv.Itoa(int(code))
-	api.LogDebug(respCode)
-
-	/*
-		// It's possible to kick off a goroutine here.
-		// But it's unsafe to access the f.callbacks because the FilterCallbackHandler
-		// may be already released when the goroutine is scheduled.
-		go func() {
-			defer func() {
-				if p := recover(); p != nil {
-					const size = 64 << 10
-					buf := make([]byte, size)
-					buf = buf[:runtime.Stack(buf, false)]
-					fmt.Printf("http: panic serving: %v\n%s", p, buf)
-				}
-			}()
-
-			// do time-consuming jobs
-		}()
-	*/
-}
-
-// OnLogDownstreamStart is called when HTTP Connection Manager filter receives a new HTTP request
-// (required the corresponding access log type is enabled)
-func (f *filter) OnLogDownstreamStart() {
-	// also support kicking off a goroutine here, like OnLog.
-}
-
-// OnLogDownstreamPeriodic is called on any HTTP Connection Manager periodic log record
-// (required the corresponding access log type is enabled)
-func (f *filter) OnLogDownstreamPeriodic() {
-	// also support kicking off a goroutine here, like OnLog.
-}
-
-func (f *filter) OnDestroy(reason api.DestroyReason) {
-	// One should not access f.callbacks here because the FilterCallbackHandler
-	// is released. But we can still access other Go fields in the filter f.
-
-	// goroutine can be used everywhere.
 }
